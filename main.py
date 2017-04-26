5a063fa76486c17bf1414075c735c06a806f3c9f
Li SH <li012589li@outlook.com>
2017-04-26 16:55:57 +0800

    add settings

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from setting import Settings
 import urllib
 
+settingPath = './setting.txt'
 
 
 def main():
+    settings = Settings(settingPath)
     url = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1'
     data = urllib.urlopen(url).read()
     print data
